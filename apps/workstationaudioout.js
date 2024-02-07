73752e0ae37de5325db8c0adc6d64e98f59d080a
izyware.admin <contact@izyware.com>
2024-02-07 23:52:39 +0000

    7200011: update service app to use service apis

--- apps/workstationaudioout.js
+++ apps/workstationaudioout.js
@@ -1,7 +1,7 @@
-process.argv[5] = `${__dirname}/../../queryObject.${process.argv[2] ? process.argv[2] + '.' : ''}xcast.json`;
+process.argv[5] = process.argv[2] ? process.argv[2] : `${__dirname}/../container-config/service-compose.json`;
 process.argv[2] = 'callpretty';
-process.argv[3] = 'vatar?service';
-process.argv[4] = 'queryObject.queryObjectId';
+process.argv[3] = 'service?start';
+process.argv[4] = 'queryObject.serviceComposeId';
 process.argv[6] = 'queryObject.service';
 process.argv[7] = 'workstationaudioout@peer';
 require('izy-proxy/cli');
